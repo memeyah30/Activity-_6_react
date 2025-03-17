ceafae6db50aa2346e7fcc5bce41ae1e4d6dd770
ruffasapan <ruffasapan@gmail.com>
2025-03-17 03:47:22 +0000

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+      <div>
+            <h1>Ruffa Mae S. Sapan</h1>
+                  <h2>Section - C</h2>
+                      </div>
+                        );
+                        }
+
+                        export default App;
+                        
